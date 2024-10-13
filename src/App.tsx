d50e51ae7fa3843b5746915c54df7f4512ebe86c
Ibrahim Abdullahi <ibraheemedrys@gmail.com>
2024-10-13 07:43:49 -0700

    add index.d.ts for .png module typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, ImageSourcePropType, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import metroConfig from './metro.config';
+import {PropsWithChildren} from 'react';
+import DiceOne from '../assets/One.png';
+import DiceTwo from '../assets/Two.png';
+import DiceThree from '../assets/Three.png';
+import DiceFour from '../assets/Four.png';
+import DiceFive from '../assets/Five.png';
+import DiceSix from '../assets/Six.png';
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
+}>;
+
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  );
+};
 
 const App = () => {
   return (
